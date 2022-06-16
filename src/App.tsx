72672718034dae74f24cb8a0bd1b75444a28f27a
sik1j <karkisikij2@gmail.com>
2022-06-15 21:59:20 -0700

    remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
